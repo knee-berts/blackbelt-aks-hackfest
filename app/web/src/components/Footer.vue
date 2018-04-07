d7cea81d729c2398b0b3027c761cc41a281df1b6
Nicholas Eberts <nicholas.eberts@yahoo.com>
2018-04-06 23:35:27 -0400

    add brigade.js

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Azure Lover Kubernetes
       </div>
       <div class="col-lg-6">
       </div>
