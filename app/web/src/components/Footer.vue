ed95948e55f0f753e508169dbbd006723e1b6fe7
Nicholas Eberts <nicholas.eberts@yahoo.com>
2018-04-08 14:15:08 -0400

    Changed Footer

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Microsoft loves kubernetes
+        Kubernetes and MS sitting in trees
       </div>
       <div class="col-lg-6">
       </div>
